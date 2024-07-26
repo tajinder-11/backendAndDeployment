3cab70a4f8926f0fb0221628280aed4a0c1208a5
tajinder-11 <dheertajinder@gmail.com>
2024-07-26 23:53:32 +0530

    added github data

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
+const data = {
+  name: 'tajinder',
+  age: 20,
+  work: 'softwate engineer',
+  location: 'punjab',
+};
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
@@ -17,6 +24,10 @@ app.get('/login', (req, res) => {
 
 app.get('/backend', (req, res) => {
   res.send(`<h1>This is Backend course</h1>`);
+});
+
+app.get('/tajinder', (req, res) => {
+  res.json(data);
 });
 
 app.listen(port, () => {
